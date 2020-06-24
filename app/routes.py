048d6722bfdbe66fe61caa72fa50d56b340adab6
17trubin@gmail.com <17trubin@gmail.com>
2020-06-24 16:10:47 +0300

    Implemented pagination

--- app/routes.py
+++ app/routes.py
@@ -11,23 +11,28 @@ from app import db
 from app.forms import RegistrationForm
 from app.forms import EditProfileForm
 from app.forms import EmptyForm
+from app.forms import PostForm
+from app.models import Post
 from datetime import datetime
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/index', methods = ['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body = form.post.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home Page', form=form, posts=posts.items, next_url = next_url, prev_url = prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -46,15 +51,6 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form = form)
 
-
-    # Old fake version of login
-    # form = LoginForm()
-    # if form.validate_on_submit():
-    #     flash('Login requested for user {}, remember_me={}'.format(
-    #         form.username.data, form.remember_me.data))
-    #     return redirect(url_for('index'))
-    # return render_template('login.html', title = 'Sign In', form = form)
-    
 @app.route('/logout')
 def logout():
     logout_user()
@@ -78,12 +74,14 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts.items, next_url = next_url, prev_url = prev_url, form=form)
 
 @app.route('/edit_profile', methods=["GET", 'POST'])
 @login_required
@@ -137,10 +135,20 @@ def unfollow(username):
         if user == current_user:
             flash('You cannot unfollow yourself!')
             return redirect(url_for('user', username=username))
-        current_user.unfullow(user)
+        current_user.unfollow(user)
         db.session.commit()
         flash('You are not following {}.'.format(username))
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
 
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type = int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url = next_url, prev_url = prev_url)
