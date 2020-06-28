b0417f88162fc3ebd96756b83b43adfe5e941d04
17trubin@gmail.com <17trubin@gmail.com>
2020-06-28 20:42:02 +0300

    Implemented password reseting vie emails

--- app/forms.py
+++ app/forms.py
@@ -49,3 +49,11 @@ class PostForm(FlaskForm):
     post = TextAreaField('Say somethings', validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
     
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
