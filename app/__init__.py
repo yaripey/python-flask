e75e0a29b736a2e879476d656bc2f82542eceddf
17trubin@gmail.com <17trubin@gmail.com>
2020-06-30 23:32:34 +0300

    Implemented test-version of translation

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +20,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -47,3 +52,8 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 from app import routes, models, errors
+
+@babel.localeselector
+def get_locale():
+    # return request.accept_lanugages.best_match(app.config['LANGUAGES'])
+    return 'ru'
